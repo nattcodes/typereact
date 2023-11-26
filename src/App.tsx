715a864dd4df2a281000e7944c5146c17d65ccde
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-11-27 00:21:04 +0100

    w

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback, MouseEvent, useMemo } from "react"
 
 interface User {
   id: number,
   username: string
 }
+
+type FibFunc = (n: number) => number
+
+const fib: FibFunc = (n) => {
+  if (n < 2) return n
+  return fib(n - 1) + fib(n - 2)
+}
+
+const myNum: number = 37;
 
 function App() {
 
@@ -12,14 +21,25 @@ function App() {
 
   useEffect(() => {
     console.log('first');
+    console.log('Users: ', users)
+    console.log(result)
 
     return() => console.log("second")
-  }, [])
+    
+    
+    
+  }, [users])
 
+ 
+  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement>): void => setCount( count + 1 ), [])
+
+  const result = useMemo<number>(() => fib(myNum), [myNum])
 
   return(
-    <>
-    </>
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+    </div>
   )
 }
 
