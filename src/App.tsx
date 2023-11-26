8cab14aba87c18da3b4689db07035335aec32a74
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-11-26 22:16:26 +0100

    kd

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 
-interface User {
-  id: number,
-  username: string
-}
+// interface User {
+//   id: number,
+//   username: string
+// }
 
 function App() {
-  const [count, setCount] = useState<number>(0)
-  const [users, setUsers] = useState<User[] | null>(null)
+  const [userData, setUserData] = useState<any>([])
 
   useEffect(() => {
-    console.log('first');
-
-    return() => console.log("second")
+    fetcher()
   }, [])
+
+  const fetcher = () => {
+    axios.get('https://randomuser.me/api')
+    .then((data) => {
+      setUserData(JSON.stringify(data, null, 2))
+  })
+
+}
+
+  // const [count, setCount] = useState<number>(0)
+  // const [users, setUsers] = useState<User[] | null>(null)
+
+  // useEffect(() => {
+  //   console.log('first');
+
+  //   return() => console.log("second")
+  // }, [])
+  //https://randomuser.me/api
 
   return(
     <>
+      <h1>Fetch Api</h1>
+      <p>{userData}</p>
       
     </>
   )
